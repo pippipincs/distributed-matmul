823910af772b7980fc476fb4fcbc1462f2e7d1ab
PPIP <agnesspei731@gmail.com>
2026-04-19 02:31:38 -0500

    cannon's algo

--- HW5/optimized_kernel.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void batched_pairwise_optimized(const float *d_As, const float *d_Bs,
-                                float *d_C_sum,
-                                int numA, int numB,
-                                int M, int N, int K);
